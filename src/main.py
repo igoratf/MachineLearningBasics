d5fd35e94fbbd44808fddc1e887cacddac6dd0e0
igor <igor.atf@gmail.com>
2019-10-26 16:15:04 -0300

    add model building code

--- src/main.py
+++ src/main.py
@@ -46,3 +46,30 @@ for i in range(25):
     plt.xlabel(class_names[train_labels[i]])
 plt.show()
 
+
+## Build the model
+
+
+# Set up the layers
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28, 28)),
+    keras.layers.Dense(128, activation='relu'),
+    keras.layers.Dense(10, activation='softmax')
+])
+
+# Compile the model
+
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+# Train the model
+
+model.fit(train_images, train_labels, epochs=10)
+
+# Evaluate accuracy
+
+test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
+
+print('\nTest accuracy:', test_acc)
+
